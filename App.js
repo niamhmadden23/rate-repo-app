e7e2f58b8910fdd70523af95e62f8b4afcf16c51
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-04-19 20:48:58 +0100

    added apollo client

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ApolloProvider } from "@apollo/client";
 import { NativeRouter } from "react-router-native";
 import Main from "./src/components/Main";
+import createApolloClient from "./src/utils/apolloClient";
+
+const apolloClient = createApolloClient();
 
 export default function App() {
   return (
     <NativeRouter>
-      <Main />
+      <ApolloProvider client={apolloClient}>
+        <Main />
+      </ApolloProvider>
     </NativeRouter>
   );
 }
